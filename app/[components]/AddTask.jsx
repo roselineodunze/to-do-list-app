0248e990346182a7f10bbee0cf5598279a33360b
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2025-02-16 18:28:10 +0100

    error handling

--- app/[components]/AddTask.jsx
+++ app/[components]/AddTask.jsx
@@ -1,20 +1,21 @@
 "use client";
 import React, { useState, useContext, useEffect } from "react";
-import { Send, Add } from "iconsax-react";
+import { Send, Add, CloseCircle } from "iconsax-react";
 import useTaskStore from "../stores/taskStore";
-
 
 const AddTask = () => {
   const [title, setTitle] = useState("");
-  const { addTask, updateTitle, isEdit, setIsEdit, taskToEdit, setTaskToEdit } = useTaskStore();
+  const { addTask, updateTitle, isEdit, setIsEdit, taskToEdit, setTaskToEdit } =
+    useTaskStore();
+  const [titleError, setTitleError] = useState(false);
 
   useEffect(() => {
     if (isEdit) {
-      if(taskToEdit){
-        setTitle(taskToEdit.title)
+      if (taskToEdit) {
+        setTitle(taskToEdit.title);
       }
-    } 
-  }, [isEdit, taskToEdit])
+    }
+  }, [isEdit, taskToEdit]);
 
   const handleAddTask = async () => {
     const newTask = {
@@ -27,15 +28,20 @@ const AddTask = () => {
 
   const handleUpdateTask = () => {
     console.log("updating" + title);
-    if (taskToEdit){
-      updateTitle(taskToEdit.id, title)
+    if (taskToEdit) {
+      updateTitle(taskToEdit.id, title);
     }
     setTitle("");
-    setIsEdit(false)
-    setTaskToEdit(null)
+    setIsEdit(false);
+    setTaskToEdit(null);
   };
 
   const handleSubmit = () => {
+    if (!title) {
+      setTitleError(true);
+
+      return;
+    }
     if (isEdit) {
       handleUpdateTask();
     } else {
@@ -44,28 +50,38 @@ const AddTask = () => {
   };
 
   return (
-    <div className="mt-6 flex items-center gap-3">
-      <input
-        type="text"
-        placeholder="add a task"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        className="bg-gray-900 text-white rounded-md p-2 flex-grow"
-      />
-      <button
-        className="bg-green-600 p-2 flex justify-center items-center rounded-md"
-        onClick={handleSubmit}
-      >
-        {isEdit ? (
-          <Send
-            size="30"
-            color="#ffffff"
-          />
-        ) : (
-          <Add size="32" color="#ffffff" />
-        )}
-      </button>
-    </div>
+    <>
+      {titleError ? (
+        <div className="bg-red-500 mt-6 p-2 text-black flex justify-between items-center rounded-sm">
+          <p>Please enter a task. 🙂</p>
+          <button onClick={() => setTitleError(false)}>
+            {" "}
+            <CloseCircle size="28" color="#000000" />
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="mt-6 flex items-center gap-3">
+        <input
+          type="text"
+          placeholder="Add a task"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="bg-gray-900 text-white rounded-md p-2 flex-grow"
+        />
+        <button
+          className="bg-green-600 p-2 flex justify-center items-center rounded-md"
+          onClick={handleSubmit}
+        >
+          {isEdit ? (
+            <Send size="30" color="#ffffff" />
+          ) : (
+            <Add size="32" color="#ffffff" />
+          )}
+        </button>
+      </div>
+    </>
   );
 };
 
--- app/[components]/TaskManager.jsx
+++ app/[components]/TaskManager.jsx
@@ -15,7 +15,7 @@ const TaskManager = () => {
       <div className="flex items-center border p-5">
         <div className="w-[50%] text-white flex flex-col items-center">
           <h1 className="sm:text-6xl text-3xl">Task Done</h1>
-          <h1 className="sm:text-4xl text-xl">Keep it up</h1>
+          <h1 className="sm:text-4xl text-xl">Keep it up!</h1>
         </div>
         <div className="w-[50%] flex justify-center items-center">
           <div className="bg-green-600 sm:h-40 sm:w-40 w-32 h-32 rounded-full flex justify-center items-center">
@@ -29,7 +29,7 @@ const TaskManager = () => {
       <div className="h-50% h-[40vh] overflow-scroll mt-7 mb-2 flex flex-col">
         {tasks.length === 0 ? (
           <div className="text-center text-sm">
-            <h1 className="text-gray-300">No tasks added yet.</h1>
+            <h1 className="text-gray-300">No task added yet.</h1>
           </div>
         ) : (
           tasks.map((task, i) => (
